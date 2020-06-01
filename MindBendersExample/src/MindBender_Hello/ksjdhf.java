8760b685373a0a051300eba3dd62031f021ab62c
svgnhkmglu <sevginhekimoglu@gmail.com>
2020-05-31 21:23:31 -0400

    asd

--- MindBendersExample/src/MindBender_Hello/ksjdhf.java
+++ MindBendersExample/src/MindBender_Hello/ksjdhf.java
@@ -3,5 +3,9 @@ package MindBender_Hello;
 public class ksjdhf {
     public static void main(String[] args) {
         System.out.println("run");
+
+        System.out.println("hello tuba");
     }
+
+
 }
